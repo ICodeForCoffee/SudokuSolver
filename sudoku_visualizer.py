e3ce2b6f50811ba444fbe39aabe5c7bb2fc62ea3
Matthew Pouliotte <poul102@gmail.com>
2026-01-11 16:11:53 -0500

    Upated sudoku_visualizer nicegui calls.

--- sudoku_visualizer.py
+++ sudoku_visualizer.py
@@ -283,11 +283,11 @@ class SudokuVisualizer:
         app.native.start_args['debug'] = True
         app.native.settings['ALLOW_DOWNLOADS'] = True
         
-        ui.html(STYLE, sanitize=False)
+        ui.html(STYLE)
         ui.label('Hello NiceGUI!')
         
         sudoku_rendering = self.generate_sudoku_render(puzzle)
-        ui.html(sudoku_rendering, sanitize=False)
+        ui.html(sudoku_rendering)
         
         ui.button('Solve Puzzle', on_click=lambda: ui.notify('button was pressed'))
         
@@ -301,10 +301,10 @@ class SudokuVisualizer:
             app.native.start_args['debug'] = True
             app.native.settings['ALLOW_DOWNLOADS'] = True
         
-        ui.html(STYLE, sanitize=False)
+        ui.html(STYLE)
         ui.page_title = "Sudoku"
         
-        container = ui.html(sanitize=False)
+        container = ui.html()
         
         global timer
         timer = ui.timer(1.0, lambda: container.set_content(get_next_step(steps)), active=False)
